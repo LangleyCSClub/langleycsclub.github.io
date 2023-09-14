7d69b08edacb427d810a4b417131cf26cd71300e
Sir Blob <76974209+GamerBoss101@users.noreply.github.com>
2023-09-13 21:49:34 -0400

    Web Start

---
+++ _app/immutable/nodes/1.303645df.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.b732239f.js";import{S as t,i as r}from"../chunks/index.547d5808.js";class l extends t{constructor(o){super(),r(this,o,null,null,s,{})}}export{l as component};
---
+++ _app/immutable/nodes/4.73e91d78.js
@@ -0,0 +1,9 @@
+import{s as i,n as a}from"../chunks/scheduler.b732239f.js";import{S as r,i as l,g as u,h as c,x as p,k as h,a as d,f as g}from"../chunks/index.547d5808.js";function b(s){let t,o=`<p class="title svelte-12cebfm">Gitpod</p> <p style="font-size: 18px; width:80%;">Gitpod a cloud-based IDE that allows you to code in the browser. 
+        It is a great tool for beginners to get started with coding.
+        It&#39;s a free tool that only requires a Github account to get started.
+        It uses a linux container that allows you to run any language you want.</p> <p>1. To get started, you have to create a Github account.<br/>
+        2. If you already have a Github account, you can skip this step.<br/>
+        3. Then create a Blank Github Repository.<br/>
+        4. Link your Github account to Gitpod.<br/>
+        5. Open your Github Repository in Gitpod.<br/>
+        6. Start coding!</p> <img width="80%" src="/gitpod-ide.png" alt="Gitpod IDE"/> <br/> <button><a href="https://www.gitpod.io">Open Gitpod</a></button>`;return{c(){t=u("div"),t.innerHTML=o,this.h()},l(e){t=c(e,"DIV",{class:!0,"data-svelte-h":!0}),p(t)!=="svelte-1n890l9"&&(t.innerHTML=o),this.h()},h(){h(t,"class","container")},m(e,n){d(e,t,n)},p:a,i:a,o:a,d(e){e&&g(t)}}}class m extends r{constructor(t){super(),l(this,t,null,b,i,{})}}export{m as component};
